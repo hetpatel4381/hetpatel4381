387fd09a8db2673f665e2949268f61066ef814a9
Het <hetpatel2808hp@gmail.com>
2022-11-22 00:54:30 +0530

    arrays with java day 1

---
+++ lineraSearch.java
@@ -0,0 +1,29 @@
+public class lineraSearch {
+    public static void main(String[] args) {
+        int[] arr = {11, 22, 33, 44, 55};
+        int target = 33;
+        System.out.println(solve(arr, target, 0));
+        System.out.println(findIndex(arr, target, 0));
+        System.out.println(reverseIndex(arr, target, arr.length - 1));
+    }
+
+    // it is returning the boolean value as if the value is their or not
+    static boolean solve(int[] arr, int target, int index){
+        if(index == arr.length) return false;
+        return arr[index] == target || solve(arr, target, index + 1);
+    }
+
+    //it is returning the index at which the element lies
+    static int findIndex(int[] arr, int target, int index){
+        if(index == arr.length) return -1;
+        if(arr[index] == target) return index;
+        else return findIndex(arr, target, index + 1);
+    }
+    
+    static int reverseIndex(int[] arr, int target, int index){
+        if(index == -1) return -1;
+        if(arr[index] == target) return index;
+        else return findIndex(arr, target, index - 1);
+    }
+
+}
