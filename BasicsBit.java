bbb02cdf7c13b4680bb074b893ef3642c4b7d5ee
Het <hetpatel2808hp@gmail.com>
2022-11-30 23:55:42 +0530

    adding BitManipulation Folder

---
+++ BasicsBit.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BasicsBit {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = s.nextInt();
+        int j = s.nextInt();
+        int k = s.nextInt();
+        int m = s.nextInt();
+
+        //preparing mask for bitmanipulation
+        int onMask = (1 << i);
+        int offMask = ~(1 << j);
+        int toggleMask = (1 << k);
+        int checkMask = (1 << m);
+
+        //printing the output as we needed
+        System.out.println(n | onMask);
+        System.out.println(n & offMask);
+        System.out.println(n ^ toggleMask);
+        System.out.println((n & checkMask) == 0 ? false : true);
+    }
+}
